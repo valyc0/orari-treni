2abc2269ce10dfcc94b88d9588eb378cf4029dd0
valerio <valyc0@gmail.com>
2026-05-07 00:17:46 +0200

    Fix PWA: percorsi relativi per deploy in sottocartella

--- sw.js
+++ sw.js
@@ -1,11 +1,10 @@
 const CACHE = 'treni-v1';
 const STATIC = [
-  '/',
-  '/index.html',
-  '/treni.js',
-  '/manifest.json',
-  '/icons/icon-192.png',
-  '/icons/icon-512.png',
+  './index.html',
+  './treni.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
 ];
 
 self.addEventListener('install', e => {
